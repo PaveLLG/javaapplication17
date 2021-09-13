1b7b7ed262f8d37a6beaab054a2d3622fbf79cd6
PaveL <BPaveL07@yandex.ru>
2021-09-13 21:25:42 +0300

    1111

--- src/JavaApplication17.java
+++ src/JavaApplication17.java
@@ -1,6 +1,21 @@
 public class JavaApplication17 {
 
     public static void main(String[] args) {
-
+        FSServer server = new FSServer(".");
+        server.addClient(new FSMonitor() {
+            @Override
+            public void event(String fName, int kind) {
+                switch (kind){
+                    case FSMonitor.CREATE:
+                        System.out.println("File create: " + fName);
+                        break;
+                    case  FSMonitor.REMOVE:
+                        System.out.println("File removed: "+  fName);
+                        break;
+                    default:System.out.println("Unknown event!");
+                }
+            }
+        });
+        server.start();
     }
 }
